36e175070f16099a4c27860af7cddf33ce7add2b
sldstrst <bog7673@gmail.com>
2022-08-06 15:51:51 +0300

    reviews Kata v1

--- src/main/java/ru/home/test/Main.java
+++ src/main/java/ru/home/test/Main.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        String result;
         boolean flag = true;
 
         while (flag) {
@@ -13,8 +14,20 @@ public class Main {
             if (expression.equals("Exit")) {
                 flag = false;
             }else {
-                new СalculatorNumbers(expression);
+                result = calc(expression);
+                System.out.println(result);
             }
         }
     }
+
+    public static String calc(String input){
+        CalculatorNumbers calc = new CalculatorNumbers();
+        try {
+            input = calc.getExpression(input);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return input;
+    }
+
 }
